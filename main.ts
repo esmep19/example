31857d510ec3c2f255394de3c5279bfb98f29246
esmep19 <74425361+esmep19@users.noreply.github.com>
2020-11-13 09:50:33 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        # . # . .
+        . # . # .
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        . # . . #
+        `)
+    basic.showLeds(`
+        . . # . #
+        . # # # .
+        # . # . .
+        . # . # .
+        . # . # .
+        `)
+    basic.showString("Yay Ms. Padilla!")
+    basic.showIcon(IconNames.Giraffe)
+})
